c45c391b12b87f5b8163f87d84b04466f2022b6f
3y3 <3y3@bk.ru>
2013-05-13 07:14:15 +0400

    Interface Extend

--- main.js
+++ main.js
@@ -33,26 +33,120 @@
 	}(DOMParser));
 	/*END DOMParser polyfill*/
 	var d = document;
+	var a = 'appendChild';
 	var q = 'querySelector';
 	var s = 'style';
-	this.interface = new function(){
-		var DOMP = new DOMParser();
-		var commbar = d[q]('#commbar');
-		var commbarContainer = commbar[q]('.jxBarContainer');
-		
-		var tradeSrc = '<span class="jxButtonContainer" id="addon-trade-panel">'
-			+'<a class="jxButton jxButtonFlyout jxDiscloser" target="" href="javascript:void(0);" title="Информация по лучшим ценам на ресурсы с посещенных вами планет." alt="Информация по лучшим ценам на ресурсы с посещенных вами планет.">'
-				+'<span class="jxButtonContent">'
-					+'<img class="jxButtonIcon" src="http://game.far7.by/static/img/u/a_pixel.png" title="Информация по лучшим ценам на ресурсы с посещенных вами планет." alt="Информация по лучшим ценам на ресурсы с посещенных вами планет." style="background-image: url(http://game.far7.by/static/img/u/icon-trade.png);"/>'
-					+'<span class="jxButtonLabel" style="">'
-						+'<div id="addon-trade">Торговля</div>'
-		+'</span></span></a></span>';
-		this.tradeButton = DOMP.parseFromString(tradeSrc,'text/html').documentElement;
-		//TODO: Compute new width
-		commbar[s].width = '1000px';
-		commbarContainer.appendChild(this.tradeButton);
-	}
 	this.trade = new function tradeProcess(){
+		var trade = this;
+		this.interface = new function(){
+				var interfase = this;
+				var DOMP = new DOMParser();
+				var far = d[q]('#far-interface');
+				var commbar = d[q]('#commbar');
+				var commbarContainer = commbar[q]('.jxBarContainer');
+			
+				var tradeSrc = '<span class="jxButtonContainer" id="addon-trade-panel">'
+					+'<a class="jxButton jxButtonFlyout jxDiscloser" target="" href="javascript:void(0);" title="Информация по лучшим ценам на ресурсы с посещенных вами планет." alt="Информация по лучшим ценам на ресурсы с посещенных вами планет.">'
+						+'<span class="jxButtonContent">'
+							+'<img class="jxButtonIcon" src="http://game.far7.by/static/img/u/a_pixel.png" title="Информация по лучшим ценам на ресурсы с посещенных вами планет." alt="Информация по лучшим ценам на ресурсы с посещенных вами планет." style="background-image: url(http://game.far7.by/static/img/u/icon-trade.png);"/>'
+							+'<span class="jxButtonLabel" style="">'
+								+'<div id="addon-trade">Торговля</div>'
+				+'</span></span></a></span>';
+			this.tradeButton = DOMP.parseFromString(tradeSrc,'text/html').documentElement;
+				this.tradeButton.addEventListener('click', displayDialog, false);
+				commbarContainer[a](this.tradeButton);
+				commbar[s].width = '1000px';
+			
+				var rect = this.tradeButton.getClientRects()[0];
+				var dialodSrc =
+					'<div class="jxFlyout jxHasChrome" style="left: '+(rect.left - 5)+'px; top: 25px; z-index: 1000;width: 670px; height: 375px;display: none;">'
+						+'<div class="jxFlyoutContent"></div>'
+						+'<div class="jxChrome" style="padding: 0px; background-image: none; z-index: -1;"><div class="jxChromeTR"><img class="png24" src="http://game.far7.by/static/img/u/chrome-flyout.png" alt="" title=""></div><div class="jxChromeTL"><img class="png24" src="http://game.far7.by/static/img/u/chrome-flyout.png" alt="" title=""></div><div class="jxChromeBL"><img class="png24" src="http://game.far7.by/static/img/u/chrome-flyout.png" alt="" title=""></div><div class="jxChromeBR"><img class="png24" src="http://game.far7.by/static/img/u/chrome-flyout.png" alt="" title=""></div></div>'
+					+'</div>';
+				var tradeDialog = DOMP.parseFromString(dialodSrc,'text/html').documentElement;
+			this.tradeDialog = tradeDialog;
+			
+				var tradeDialogContentSrc = '<div id="landing-trade"><div id="landing-trade-shp"><div id="s1" class="s1 trade"><img id="is1" isid="1" src="/static/img/i/r/tore.png" class="dragable animopa itemtip iundefined is1 shopitem" slot="s1" itype="i" useit="0" subtype="tore" lvl="0" cls="" price="100" title="" style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is1c" class="count is1c" style="visibility: visible;">-</div><div id="is1sell" class="count sellprice is1sell"></div><div id="is1buy" class="count buyprice is1buy"></div><div id="is1arrow" class="tradearrow arrow0"></div></div><div id="s2" class="s2 trade"><img id="is2" isid="2" src="/static/img/i/r/tfoo.png" class="dragable animopa itemtip iundefined is2 shopitem" slot="s2" itype="i" useit="0" subtype="tfoo" lvl="0" cls="" price="200" buy="148" sell="296" count="600" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is2c" class="count is2c" style="visibility: visible;">-</div><div id="is2sell" class="count sellprice is2sell"></div><div id="is2buy" class="count buyprice is2buy"></div><div id="is2arrow" class="tradearrow arrow0"></div></div><div id="s3" class="s3 trade"><img id="is3" isid="3" src="/static/img/i/r/tfue.png" class="dragable animopa itemtip iundefined is3 shopitem" slot="s3" itype="i" useit="0" subtype="tfue" lvl="0" cls="" price="500" buy="603" sell="1307" count="100" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is3c" class="count is3c" style="visibility: visible;">-</div><div id="is3sell" class="count sellprice is3sell"></div><div id="is3buy" class="count buyprice is3buy"></div><div id="is3arrow" class="tradearrow arrow0"></div></div><div id="s4" class="s4 trade"><img id="is4" isid="4" src="/static/img/i/r/tmin.png" class="dragable animopa itemtip iundefined is4 shopitem" slot="s4" itype="i" useit="0" subtype="tmin" lvl="0" cls="" price="1000" buy="1715" sell="0" count="236" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is4c" class="count is4c" style="visibility: visible;">-</div><div id="is4sell" class="count sellprice is4sell"></div><div id="is4buy" class="count buyprice is4buy"></div><div id="is4arrow" class="tradearrow arrow0"></div></div><div id="s5" class="s5 trade"><img id="is5" isid="5" src="/static/img/i/r/tmat.png" class="dragable animopa itemtip iundefined is5 shopitem" slot="s5" itype="i" useit="0" subtype="tmat" lvl="0" cls="" price="3000" buy="5237" sell="0" count="215" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is5c" class="count is5c" style="visibility: visible;">-</div><div id="is5sell" class="count sellprice is5sell"></div><div id="is5buy" class="count buyprice is5buy"></div><div id="is5arrow" class="tradearrow arrow0"></div></div><div id="s6" class="s6 trade"><img id="is6" isid="6" src="/static/img/i/r/talc.png" class="dragable animopa itemtip iundefined is6 shopitem" slot="s6" itype="i" useit="0" subtype="talc" lvl="0" cls="" price="7000" buy="13814" sell="0" count="110" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is6c" class="count is6c" style="visibility: visible;">-</div><div id="is6sell" class="count sellprice is6sell"></div><div id="is6buy" class="count buyprice is6buy"></div><div id="is6arrow" class="tradearrow arrow0"></div></div><div id="s7" class="s7 trade"><img id="is7" isid="7" src="/static/img/i/r/tmed.png" class="dragable animopa itemtip iundefined is7 shopitem" slot="s7" itype="i" useit="0" subtype="tmed" lvl="0" cls="" price="15000" buy="10944" sell="21889" count="653" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is7c" class="count is7c" style="visibility: visible;">-</div><div id="is7sell" class="count sellprice is7sell"></div><div id="is7buy" class="count buyprice is7buy"></div><div id="is7arrow" class="tradearrow arrow0"></div></div><div id="s8" class="s8 trade"><img id="is8" isid="8" src="/static/img/i/r/ttch.png" class="dragable animopa itemtip iundefined is8 shopitem" slot="s8" itype="i" useit="0" subtype="ttch" lvl="0" cls="" price="25000" buy="22529" sell="48812" count="540" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is8c" class="count is8c" style="visibility: visible;">-</div><div id="is8sell" class="count sellprice is8sell"></div><div id="is8buy" class="count buyprice is8buy"></div><div id="is8arrow" class="tradearrow arrow0"></div></div><div id="s9" class="s9 trade"><img id="is9" isid="9" src="/static/img/i/r/tart.png" class="dragable animopa itemtip iundefined is9 shopitem" slot="s9" itype="i" useit="0" subtype="tart" lvl="0" cls="" price="40000" buy="72241" sell="0" count="176" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is9c" class="count is9c" style="visibility: visible;">-</div><div id="is9sell" class="count sellprice is9sell"></div><div id="is9buy" class="count buyprice is9buy"></div><div id="is9arrow" class="tradearrow arrow0"></div></div><div id="s10" class="s10 trade"><img id="is10" isid="10" src="/static/img/i/r/tgdt.png" class="dragable animopa itemtip iundefined is10 shopitem" slot="s10" itype="i" useit="0" subtype="tgdt" lvl="0" cls="" price="55000" buy="50143" sell="108644" count="500" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is10c" class="count is10c" style="visibility: visible;">-</div><div id="is10sell" class="count sellprice is10sell"></div><div id="is10buy" class="count buyprice is10buy"></div><div id="is10arrow" class="tradearrow arrow0"></div></div><div id="s11" class="s11 trade"><img id="is11" isid="11" src="/static/img/i/r/tdrg.png" class="dragable animopa itemtip iundefined is11 shopitem" slot="s11" itype="i" useit="0" subtype="tdrg" lvl="0" cls="" price="80000" buy="153763" sell="0" count="130" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is11c" class="count is11c" style="visibility: visible;">-</div><div id="is11sell" class="count sellprice is11sell"></div><div id="is11buy" class="count buyprice is11buy"></div><div id="is11arrow" class="tradearrow arrow0"></div></div><div id="s12" class="s12 trade"><img id="is12" isid="12" src="/static/img/i/r/twea.png" class="dragable animopa itemtip iundefined is12 shopitem" slot="s12" itype="i" useit="0" subtype="twea" lvl="0" cls="" price="110000" buy="100286" sell="217288" count="500" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is12c" class="count is12c" style="visibility: visible;">-</div><div id="is12sell" class="count sellprice is12sell"></div><div id="is12buy" class="count buyprice is12buy"></div><div id="is12arrow" class="tradearrow arrow0"></div></div><div id="s13" class="s13 trade"><img id="is13" isid="13" src="/static/img/i/r/tlux.png" class="dragable animopa itemtip iundefined is13 shopitem" slot="s13" itype="i" useit="0" subtype="tlux" lvl="0" cls="" price="150000" buy="281459" sell="0" count="143" maxcount="10" title=""  style="top: 1px; left: 1px; margin: 0px; position: relative;"><div id="is13c" class="count is13c" style="visibility: visible;">-</div><div id="is13sell" class="count sellprice is13sell"></div><div id="is13buy" class="count buyprice is13buy"></div><div id="is13arrow" class="tradearrow arrow0"></div></div></div></div>';
+				var tradeDialogContent = DOMP.parseFromString(tradeDialogContentSrc,'text/html').documentElement;
+			this.tradeDialogContent = tradeDialogContent;
+				far[a](this.tradeDialog);
+			this.data = {};
+				
+				for(var i=1; i < 14; ++i){
+					(function(i){
+						var cur_mat = tradeDialogContent[q]('#s'+i);
+						var img = cur_mat[q]('#is'+i);
+						var buy = cur_mat[q]('#is'+i+'sell');
+						var sell = cur_mat[q]('#is'+i+'buy');
+						var count = cur_mat[q]('#is'+i+'c');
+						var arrow = cur_mat[q]('#is'+i+'arrow');
+						interfase.data[i] = {
+							img: {dom:img},
+							count:{dom:count},
+							sell: {dom:sell, info:{system:null,planet:null,lastupdate:null}},
+							buy: {dom:buy, info:{system:null,planet:null,lastupdate:null}},						
+							arrow:{dom:arrow}
+						}
+					})(i);
+				}
+				
+			function displayDialog(e){
+				e.stopPropagation();
+				if(tradeDialog[s].display == 'none'){
+					updateInfo();
+					interfase.tradeDialog.children[0][a](tradeDialogContent);
+					tradeDialog[s].display = 'block';
+					window.addEventListener('click', hideDialod, false);
+					interfase.tradeButton.removeEventListener('click',displayDialog);
+				}
+			}
+			function hideDialod(e){
+				e.stopPropagation();
+				if(tradeDialog[s].display == 'block'){
+					tradeDialog[s].display = 'none';
+					interfase.tradeDialog.children[0].removeChild(tradeDialogContent);
+					window.removeEventListener('click', hideDialod);
+					interfase.tradeButton.addEventListener('click', displayDialog, false);
+				}
+				return false;
+			}
+			function updateInfo(){
+			
+				for(var i in interfase.data){
+					var cur_c = tradeDialogContent[q]('#s'+i);
+					if(!cur_c[q]('#is'+i)){cur_c[a](interfase.data[i].img.dom)}
+					if(!cur_c[q]('#is'+i+'c')){cur_c[a](interfase.data[i].count.dom)}
+					if(!cur_c[q]('#is'+i+'sell')){cur_c[a](interfase.data[i].buy.dom)}
+					if(!cur_c[q]('#is'+i+'buy')){cur_c[a](interfase.data[i].sell.dom)}					
+					if(!cur_c[q]('#is'+i+'arrow')){cur_c[a](interfase.data[i].arrow.dom)}
+					var buy = trade.buy(i);
+					var sell = trade.sell(i);
+					if(buy[0]){
+						buy = buy[0];
+						interfase.data[i].buy.dom.innerHTML = buy.buy+'кр.';
+						interfase.data[i].buy.dom.setAttribute('title','Система: '+buy.system+'\nПланета: '+buy.planet+'\nОбновлено в '+buy.time.toLocaleTimeString());
+						interfase.data[i].buy.info.system = buy.system;
+						interfase.data[i].buy.info.planet = buy.planet;
+						interfase.data[i].buy.info.lastupdate = buy.time;
+					}
+					else{
+						interfase.data[i].buy.dom.innerHTML = '-';
+					}
+					if(sell[0]){
+						sell=sell[0];
+						interfase.data[i].sell.dom.innerHTML = sell.sell+'кр.';
+						interfase.data[i].sell.dom.setAttribute('title','Система: '+sell.system+'\nПланета: '+sell.planet)
+						interfase.data[i].sell.info.system = sell.system;
+						interfase.data[i].sell.info.planet = sell.planet;
+						interfase.data[i].sell.info.lastupdate = sell.time;
+					}
+					else{
+						interfase.data[i].sell.dom.innerHTML = '-';
+					}				
+				}
+			
+			}
+		}
 		this.fine = function(method, type, target){
 			
 			var current =  this.data[target];
@@ -86,6 +180,7 @@
 			(function(){
 				var land = document.querySelector('#fse-land-dialog');
 				if(land){
+					var system = fsE.sector.system.title;
 					var planet = land.querySelector('.jxDialogTitle .jxDialogLabel');
 					if(!planet.innerHTML) return;
 					var trade = land.querySelector('#landing-trade');
@@ -97,13 +192,12 @@
 						var cur_mat = mater.querySelector('#s'+i);
 						var buy = cur_mat.querySelector('#is'+i+'sell');
 						var sell = cur_mat.querySelector('#is'+i+'buy');
-						var count = cur_mat.querySelector('#is'+i+'c');
 						this.data[i] = this.data[i] || {};
 						this.data[i][planet.innerHTML] = {
+							system: system,
 							planet: planet.innerHTML,
 							sell: sell.innerHTML.replace('кр.','').replace(/\s/,'').replace('профицит','-1')*1,
 							buy: buy.innerHTML.replace('кр.','').replace(/\s/,'').replace('дефицит','-1')*1,
-							count: count.innerHTML*1,
 							time: time
 						}
 					}
