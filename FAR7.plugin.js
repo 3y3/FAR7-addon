135e91b3eb3c3021696d60952a6e116e29dd8a95
3y3 <3y3@bk.ru>
2013-06-06 07:59:09 +0400

    Revert "convert to g-shop module"
    This reverts commit 7ce2d1909227baeb0c5d4630e56b1a2ba58c49b7.

--- FAR7.plugin.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	(	document.head
-	|| 	document.body
-	|| 	document.documentElement
-		).appendChild(document.createElement('script')).src = chrome.extension.getURL('inject.js');
-}
