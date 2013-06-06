a01380b40d4d9999f9217027375844807b37e7a5
3y3 <3y3@bk.ru>
2013-06-07 01:36:11 +0400

    update

--- FAR7.plugin.js
+++ FAR7.plugin.js
@@ -2,5 +2,5 @@ window.onload = function() {
 	(	document.head
 	|| 	document.body
 	|| 	document.documentElement
-		).appendChild(document.createElement('script')).src = chrome.extension.getURL('inject.js');
+		).appendChild(document.createElement('script')).src = chrome.extension.getURL('FAR7.source.js');
 }
